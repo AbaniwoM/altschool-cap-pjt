d7f8c2415e28f9633b5e801828c42cae65a56e1f
Michael Abaniwo <michaelabaniwo@gmail.com>
2023-06-25 23:02:51 +0100

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIwNzY5Mg/spectral.js?branch_slug=main&token=84521e4d-d9d1-432f-85a6-ca9f61b18e1f";
+export default { extends: ruleset };
