b3aa26ccd47338d6bfe058cc1799d252e94db689
AbaniwoM <michaelabaniwo@gmail.com>
2023-07-01 16:19:23 +0100

    update app

--- db/connect.ts
+++ db/connect.ts
@@ -1,17 +1,15 @@
 const mongoose = require("mongoose");
 const { dbConfig } = require("../config/config");
 
-function connectToDb() {
+async function connectToDb() {
     mongoose.set("strictQuery", false);
-    mongoose.connect(dbConfig.MONGODB_URL);
-
-    mongoose.connection.on("connected", () => {
-        console.log("Mongodb connected successfully!");
-    });
-
-    mongoose.connection.on("error", (err) => {
-        console.log(err);
-    });
+    try {
+    await mongoose.connect(dbConfig.MONGODB_URL).then(() => {
+       console.log("Mongodb connected successfully!");
+   
+    })} catch(error) {
+       console.log(error)
+    }
 }
 
 export {
